fdd66f163963853828a00ca8173c54737f93f118
Moni <moniele.k.s@gmail.com>
2021-03-17 08:49:04 -0300

    feat(challenge 16): Destructuring Assignments and Rest Operator

--- js/challenges.js
+++ js/challenges.js
@@ -519,3 +519,115 @@ async function testandoPromisesQueDeramCertoAmem() {
 
 //testandoPromises();
 //testandoPromisesQueDeramCertoAmem();
+
+
+
+// ################### CHALLENGE 16 ###################
+//        === Destructuring Assignment ===
+
+
+// Challenge 1 
+
+/* exists already
+const user = {
+    nome: 'Usuário',
+    idade: 25,
+    endereco: {
+      rua: 'Rua central',
+      numero: 1
+    },
+    projetos: ['Projeto 1', 'Projeto 2']
+  }
+  
+*/
+
+// PART 1
+
+// testing destructuring assignments in user object
+let { nome : name_user } = user;
+let { endereco : {rua: street_user} } = user;
+let { projetos : [projeto1, projeto2] } = user;
+let { projetos : [, prop2] } = user;
+
+
+//test area
+console.log("Name : ", name_user);
+console.log("Street : ", street_user);
+console.log("Projeto 1 : ", projeto1);
+console.log("Projeto 1 : ", projeto2);
+console.log("Second Property : ", prop2);
+
+
+
+
+// PART 2
+
+
+/* exists already
+const students = [
+   {id: 1, name: 'John Doe', age: 23, scholarship: false},
+   {id: 2, name: 'Margoe Rose', age: 19, scholarship: true},
+   {id: 3, name: 'Kayle', age: 22, scholarship: true}
+]
+*/
+
+
+// testing destructuring assignments in students array
+let [student1] = students;
+let [,, student3 ] = students;
+let [,{name : student2}] = students;
+
+
+//test area
+console.log("First Student : ", student1);
+console.log("Second Student Name: ", student2);
+
+console.log("Third Student : ", student3);
+
+
+
+
+
+// Challenge 2 
+
+//print whatever it is in the parameter
+function imprimirArr(...rest) {
+  console.log(...rest);
+
+}
+
+//destructuring objects through function parameter
+function destruct({nome: name}) {
+  console.log("Name :", name);
+}
+
+//combining two objects through function parameter
+function combineObjects({...obj1}, {...obj2}) {
+  const newObj = {
+    obj1,
+    obj2
+  }
+  console.log(newObj);
+}
+
+
+//concat 2 arrays elements with rest operator
+function concatArrays([...arr1], [...arr2]) {
+  const newArr = arr1.concat(arr2);
+  console.log(newArr);
+}
+
+
+// testing area
+
+// testing with strings
+imprimirArr("amo", "voce", "shrek");
+
+//testing with string and numbers
+imprimirArr("amo", "voce", "shrek", 1000, "vezes");
+
+destruct(user);
+
+combineObjects(user, user);
+
+concatArrays(students, students);
